e22c2254e8c7ce6c6c860d031e69687ba22c2a96
Flor <florencgmz@gmail.com>
2023-01-19 23:30:37 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NftCard from "./NftCard";
+import { CircleButton }from "./CircleButton";
+
+export {
+    HomeHeader, NftCard, FocusedStatusBar, CircleButton
+}
